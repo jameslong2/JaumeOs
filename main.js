06e75b8042c4c98ba11a85f931fe6b06b03b1040
jameslong2 <91682877+jameslong2@users.noreply.github.com>
2023-11-13 09:10:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'JaumeOs/node_modules/three/build/three.module.js';
-import { GLTFLoader } from 'JaumeOs/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'JaumeOs/node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('black');
